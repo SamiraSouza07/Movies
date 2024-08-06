d5229a43ef07b470747fe4ead58e17b852788155
SamiraSouza07 <samira.campos@germinare.org.br>
2024-08-06 07:46:28 -0300

    Subindo projeto

--- webapp/server.py
+++ webapp/server.py
@@ -11,7 +11,8 @@ def get_db_connection():
     conn = psycopg2.connect(host='maquina-backup-samirasouza.f.aivencloud.com',
                             database='dbmovies',
                             user= user_db,
-                            password = password_db)
+                            password = password_db,
+                            port = 22478)
     return conn
 
 
